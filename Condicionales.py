51b6b069a833d81dd53cd0746cbe4710681bd1ba
Luis gerardo <luishernandezroman78@gmail.com>
2025-10-27 01:05:45 -0600

    Actualizar archivo y agregar archivos condicionales y dasafio_datos

---
+++ Condicionales.py
@@ -0,0 +1,26 @@
+
+nombre = "Alura"
+
+if nombre == "Alu":
+    print("Condición verdadera")
+elif nombre == "Alura":
+    print("condición verdadera 2")
+else:
+    print("Condición falsa")
+
+
+""" Operadores de comparación
+        == igual
+        != diferente
+        > mayor a
+        < menor a
+        >= mayor o igual
+        <= menor o igual
+"""
+
+numero = 15
+
+if numero <= 15:
+    print("Condición verdadera", numero)
+else:
+    print("Condición falsa")
